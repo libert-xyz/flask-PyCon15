dd50b2583c300ce390409b6406d4b06c1e3b5220
Libert R Schmidt <katari@waynasolutions.com>
2015-12-06 16:03:53 -0500

    questions|base.html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
 proglen = ['Python','JavaScript','PHP','Ruby']
+questions = ['Is it compiled','Does it run on a VM']
 
 @app.route('/')
 def index():
@@ -14,6 +15,9 @@ def index():
 def guess(id):
 	return render_template('guess.html', guess=proglen[id])
 
+@app.route('/question/<int:id>')
+def question(id):
+	return render_template('question.html', question=questions[id])
 
 
 if __name__ == '__main__':
