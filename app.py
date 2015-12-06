bc3ef75b13c0d20f9f2600fd5849bfb824fe4d5b
rschmidtz <rschmidt@libert.xyz>
2015-12-06 01:47:21 -0500

    guess func()

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+proglen = ['Python','JavaScript','PHP','Ruby']
+
 @app.route('/')
 def index():
 	return '<h1> Hello Flask </h1>'
+
+@app.route('/guess/<int:id>')
+def guess(id):
+	return('<h1>Guess the Lenguage!</h1>'
+		'<p>My guess: {0}</p>').format(proglen[id])
+	
 
 
 if __name__ == '__main__':
