9315e8a3a12bc93d74ddf7caca40a0a940e053f8
Libert R Schmidt <katari@waynasolutions.com>
2015-12-07 10:50:43 -0500

    wtf_Forms

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 
 from flask import Flask, render_template, url_for, request, redirect
+from flask_wtf import Form
+from wtforms.fields import RadioField, SubmitField
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 
 proglen = ['Python','JavaScript','PHP','Ruby']
 questions = ['Is it compiled','Does it run on a VM']
+
+class YesNoQuestionForm(Form):
+	answer = RadioField('Your answer', choices=[('yes','Yes'), ('no', 'No')])
+	submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
@@ -17,12 +24,13 @@ def guess(id):
 
 @app.route('/question/<int:id>', methods=['GET','POST'])
 def question(id):
-	if request.method == 'POST':
-		if request.form['answer'] == 'yes':
+	form = YesNoQuestionForm()
+	if form.validate_on_submit():
+		if form.answer.data == 'yes':
 			return redirect(url_for('question', id=id+1))
 		else:
 			return redirect(url_for('question', id=id))
-	return render_template('question.html', question=questions[id])
+	return render_template('question.html', question=questions[id],form=form)
 
 
 if __name__ == '__main__':
