4ffad77096f094495951238fde01b13fad9df4c5
Libert R Schmidt <katari@waynasolutions.com>
2015-12-07 14:56:36 -0500

    learn|errors

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 
 from flask import Flask, render_template, url_for, request, redirect
 from flask_wtf import Form
-from wtforms.fields import RadioField, SubmitField
+from wtforms.fields import RadioField, SubmitField, StringField
 from guess import Guess
+from wtforms.validators import Required
 
 
 app = Flask(__name__)
@@ -23,13 +24,18 @@ class YesNoQuestionForm(Form):
 	answer = RadioField('Your answer', choices=[('yes','Yes'), ('no', 'No')])
 	submit = SubmitField('Submit')
 
+class LearnForm(Form):
+    language = StringField('What language did you pick?',
+                           validators=[Required()])
+    question = StringField('What is a question that differentiates your '
+                           'language from mine?', validators=[Required()])
+    answer = RadioField('What is the answer for your language?',
+                        choices=[('yes', 'Yes'), ('no', 'No')])
+    submit = SubmitField('Submit')
+
 @app.route('/')
 def index():
 	return render_template('index.html')
-
-@app.route('/guess/<int:id>')
-def guess(id):
-	return render_template('guess.html', guess=game.get_guess(id))
 
 @app.route('/question/<int:id>', methods=['GET','POST'])
 def question(id):
@@ -41,6 +47,26 @@ def question(id):
 		new_id = game.answer_question(form.answer.data == 'yes', id)
 		return redirect(url_for('question', id=new_id))
 	return render_template('question.html',question=question,form=form)
+
+
+@app.route('/guess/<int:id>', methods=['GET', 'POST'])
+def guess(id):
+	form = YesNoQuestionForm()
+	if form.validate_on_submit():
+		if form.answer.data == 'yes':
+			return redirect(url_for('index'))
+    	return redirect(url_for('learn', id=id))
+	return render_template('guess.html', guess=game.get_guess(id), form=form)
+
+@app.route('/learn/<int:id>', methods=['GET', 'POST'])
+def learn(id):
+	guess = game.get_guess(id)
+	form = LearnForm()
+	if form.validate_on_submit():
+	        game.expand(guess, form.language.data, form.question.data,
+                    form.answer.data == 'yes')
+        return redirect(url_for('index'))
+	return render_template('learn.html', guess=guess, form=form)
 
 if __name__ == '__main__':
 
